52895725d338a805ea76643e0954e85273c0725a
Siddalingesh Angadi <angadisiddalingesh@gmail.com>
2022-10-13 14:35:22 +0530

    Added route table and internet gateway.

--- output.tf
+++ output.tf
@@ -1,32 +1,40 @@
 output "runner_aws_access_key_id" {
-  value = aws_iam_access_key.ec2_github_runner_key.id
+  value       = aws_iam_access_key.ec2_github_runner_key.id
+  description = "The access key generated for the EC2 github runner."
 }
 
 output "runner_aws_secret_access_key" {
-  value     = aws_iam_access_key.ec2_github_runner_key.secret
-  sensitive = true
+  value       = aws_iam_access_key.ec2_github_runner_key.secret
+  sensitive   = true
+  description = "The secret key generated for the EC2 github runner."
 }
 
 output "runner_iam_role_name" {
-  value = aws_iam_role.ec2_github_runner_role.name
+  value       = aws_iam_role.ec2_github_runner_role.name
+  description = "The name of the EC2 github runner IAM role."
 }
 
 output "runner_subnet_id" {
-  value = aws_subnet.ec2_github_runner_subnet.id
+  value       = aws_subnet.ec2_github_runner_subnet.id
+  description = "ID of the EC2 github runner subnet."
 }
 
 output "runner_security_group_id" {
-  value = aws_security_group.ec2_github_runner_security_group.id
+  value       = aws_security_group.ec2_github_runner_security_group.id
+  description = "ID of the EC2 github runner security group."
 }
 
 output "ec2_runner_image_info_x86" {
-  value = aws_imagebuilder_image.ec2_github_runner_x86.output_resources
+  value       = aws_imagebuilder_image.ec2_github_runner_x86.output_resources
+  description = "x86_64 architecture image information."
 }
 
 output "ec2_runner_image_info_arm64" {
-  value = aws_imagebuilder_image.ec2_github_runner_arm64.output_resources
+  value       = aws_imagebuilder_image.ec2_github_runner_arm64.output_resources
+  description = "arm64 architecture image information."
 }
 
 output "runner_aws_region" {
-  value = var.region
+  value       = var.region
+  description = "The EC2 github runner region."
 }
