8f747adf256f9d33af1cc709d22bdcbcda48018f
Joseph Wortmann <jwortmann@quinovas.com>
2022-11-14 17:08:58 -0600

    “runner” -> “Runner”
    - moved/updated role/instance profile
    - added ami_tags for the created ami’s

--- image_builder.tf
+++ image_builder.tf
@@ -18,7 +18,7 @@ resource "aws_imagebuilder_component" "ec2_github_runner" {
   })
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_imagebuilder_image_recipe" "ec2_github_runner_arm64" {
   version      = "1.0.0"
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
@@ -66,40 +66,49 @@ resource "aws_imagebuilder_image_recipe" "ec2_github_runner_x86" {
   version      = "1.0.0"
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
-resource "aws_iam_instance_profile" "ec2_github_runner" {
-  name = "${var.resource_prefix}-ec2-github-runner"
+resource "aws_iam_role" "ec2_github_runner_imagebuilder" {
+  name = "${var.resource_prefix}-ec2-github-runner-imagebuilder"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder"
+  ]
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_instance_profile" "ec2_github_runner_imagebuilder" {
+  name = "${var.resource_prefix}-ec2-github-runner-imagebuilder"
   role = aws_iam_role.ec2_github_runner_imagebuilder.name
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
 resource "aws_imagebuilder_infrastructure_configuration" "ec2_github_runner_arm64" {
   count                         = var.architecture == "all" || var.architecture == "arm64" ? 1 : 0
-  instance_profile_name         = aws_iam_instance_profile.ec2_github_runner.name
+  instance_profile_name         = aws_iam_instance_profile.ec2_github_runner_imagebuilder.name
   name                          = "${var.resource_prefix}-ec2-github-runner-arm64"
   instance_types                = ["m6g.xlarge", "m6gd.xlarge"]
   terminate_instance_on_failure = true
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
 resource "aws_imagebuilder_infrastructure_configuration" "ec2_github_runner_x86" {
   count                         = var.architecture == "all" || var.architecture == "x86" ? 1 : 0
-  instance_profile_name         = aws_iam_instance_profile.ec2_github_runner.name
+  instance_profile_name         = aws_iam_instance_profile.ec2_github_runner_imagebuilder.name
   name                          = "${var.resource_prefix}-ec2-github-runner-x86"
   instance_types                = ["m5d.xlarge", "m6a.xlarge"]
   terminate_instance_on_failure = true
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
@@ -108,37 +117,45 @@ resource "aws_imagebuilder_distribution_configuration" "ec2_github_runner" {
   name = "ec2_github_runner"
   distribution {
     ami_distribution_configuration {
+      ami_taqs = {
+        Environment = "${var.resource_prefix}"
+        Name        = "AWS EC2 Github Runner"
+      }
       name = "ec2-github-runner-{{ imagebuilder:buildDate }}"
     }
     region = data.aws_region.current.name
   }
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner"
+    Name        = "AWS EC2 Github Runner"
   }
 }
 
 resource "aws_imagebuilder_image" "ec2_github_runner_arm64" {
-  count                            = var.architecture == "all" || var.architecture == "arm64" ? 1 : 0
+  count = var.architecture == "all" || var.architecture == "arm64" ? 1 : 0
+
   distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.ec2_github_runner.arn
   image_recipe_arn                 = aws_imagebuilder_image_recipe.ec2_github_runner_arm64[0].arn
   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.ec2_github_runner_arm64[0].arn
+
   tags = {
     Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner arm64"
+    Name        = "AWS EC2 Github Runner arm64"
   }
 }
 
 resource "aws_imagebuilder_image" "ec2_github_runner_x86" {
-  count                            = var.architecture == "all" || var.architecture == "x86" ? 1 : 0
-  distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.ec2_github_runner.arn
-  image_recipe_arn                 = aws_imagebuilder_image_recipe.ec2_github_runner_x86[0].arn
-  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.ec2_github_runner_x86[0].arn
-  tags = {
-    Environment = "${var.resource_prefix}"
-    Name        = "AWS EC2 Github runner x86"
-  }
+  count = var.architecture == "all" || var.architecture == "x86" ? 1 : 0
   depends_on = [
     aws_imagebuilder_image.ec2_github_runner_arm64[0]
   ]
+
+  distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.ec2_github_runner.arn
+  image_recipe_arn                 = aws_imagebuilder_image_recipe.ec2_github_runner_x86[0].arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.ec2_github_runner_x86[0].arn
+
+  tags = {
+    Environment = "${var.resource_prefix}"
+    Name        = "AWS EC2 Github Runner x86"
+  }
 }
