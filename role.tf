39019e3119e0d88024dbb414b473df2592186dcd
Joseph Wortmann <jwortmann@quinovas.com>
2022-11-14 17:07:50 -0600

    moved
    - policy doc to data
    - imagebuilder role to image_builder

--- role.tf
+++ role.tf
@@ -1,26 +1,3 @@
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = [
-      "sts:AssumeRole"
-    ]
-    principals {
-      identifiers = [
-        "ec2.amazonaws.com"
-      ]
-      type = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "ec2_github_runner_imagebuilder" {
-  name = "${var.resource_prefix}-ec2-github-runner-imagebuilder"
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder"
-  ]
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-}
-
 resource "aws_iam_role" "ec2_github_runner" {
   name = "${var.resource_prefix}-ec2-github-runner"
   managed_policy_arns = [
