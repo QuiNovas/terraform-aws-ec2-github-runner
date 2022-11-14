c4214922e25a7555f4081487a1eea7dfd081ec9d
Joseph Wortmann <jwortmann@quinovas.com>
2022-11-14 17:07:18 -0600

    move policy doc to data

--- data.tf
+++ data.tf
@@ -1,1 +1,15 @@
 data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      identifiers = [
+        "ec2.amazonaws.com"
+      ]
+      type = "Service"
+    }
+  }
+}
