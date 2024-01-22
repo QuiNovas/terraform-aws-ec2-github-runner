63bd40786017336548b360dea0a302dc754fa7b8
Joseph Wortmann <joseph.wortmann@gmail.com>
2024-01-22 03:16:14 +0000

    removed openssl-devel bzip2-devel libffi-devel

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   default_commands = [
     "sudo yum -y update",
-    "sudo yum -y install docker git gcc openssl-devel bzip2-devel libffi-devel",
+    "sudo yum -y install docker git gcc",
     "sudo systemctl enable docker"
   ]
   image_builder_commands = var.imagebuilder_component_commands == [] ? local.default_commands : concat(local.default_commands, var.imagebuilder_component_commands)
