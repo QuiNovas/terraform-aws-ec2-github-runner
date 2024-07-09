2247740bf9aa7f8827a6466472614c35500be96c
Joseph Wortmann <joseph.wortmann@gmail.com>
2024-07-09 20:17:51 +0000

    refactor: Update runner_home_dir and runner_tar to include var.runner_version

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
-  runner_home_dir = "/usr/local/bin/actions-runner"
-  runner_version  = "2.317.0"
-  runner_tar      = "actions-runner-linux-${var.architecture == "x86" ? "x64" : "arm64"}-${local.runner_version}.tar.gz"
+  runner_home_dir = "/usr/local/bin/actions-runner${var.runner_version}"
+  runner_tar      = "actions-runner-linux-${var.architecture == "x86" ? "x64" : "arm64"}-${var.runner_version}.tar.gz"
   default_commands = [
     "sudo yum -y update",
     "sudo yum -y install docker git gcc gcc-c++ libicu make",
@@ -9,7 +8,7 @@ locals {
     "sudo yum install -y nodejs",
     "sudo mkdir ${local.runner_home_dir}",
     "cd ${local.runner_home_dir}",
-    "sudo curl -O -L https://github.com/actions/runner/releases/download/v${local.runner_version}/${local.runner_tar}",
+    "sudo curl -O -L https://github.com/actions/runner/releases/download/v${var.runner_version}/${local.runner_tar}",
     "sudo tar xzf ${local.runner_tar}",
     "sudo rm -f ${local.runner_tar}",
     "sudo systemctl enable docker",
